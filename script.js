b883515528b445d1da05d914818cdd02f6ec0ea3
EDUARDO HENRIQUE OLIVEIRA DA SILVA <eduardo.silva416@senaisp.edu.br>
2026-03-12 11:06:54 -0300

    Quarto commit

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let inputLista = document.querySelector("#itemInput");
+let inputLT = document.querySelector("#inputLT");
 let btnAdicionar = document.querySelector("#btnAdicionar");
-let list = document.querySelector("#list");
+let list = document.querySelector("#LT");
+let message = document.querySelector("#message");
 
-btnAdicionar.addEventListener("click", function() {
-    let itemText = inputLista.value;
+btnAdicionar.addEventListener("click", () => {
 
-    let novoItem = document.createElement("li");
+    let itemTexto = inputLT.value; 
 
-    novoItem.innerText = itemText;
+    let novoItem = document.createElement("li"); 
+    novoItem.innerText = itemTexto; 
+    
+    if (itemTexto === "") {        
+        
+        message.innerText = `Campo vazio. Preencha!`;
 
-    list.appendChild(novoItem);
+        message.className = `text-danger fw-bold`;
 
-    inputLista.value = "";
+    } else {
+        
+        let messageFeedback = document.querySelector("#message");
+        
+        messageFeedback.innerText = `Item adicionado!`;
+        message.className = `text-success fw-bold`;
+        list.appendChild(novoItem); 
+    }
+    inputLT.value = ""; 
 
 });
